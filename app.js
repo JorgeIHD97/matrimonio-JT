4d0619848735497755a9d3c5065d2131433681cc
JorgeIHD97 <JORGEIVANHD@GMAIL.COM>
2026-08-08 18:12:04 -0500

    Toggle selfie class based on camera usage

--- app.js
+++ app.js
@@ -72,6 +72,11 @@ async function iniciarCamara() {
 
     video.srcObject =
       stream;
+     if (usandoFrontal) {
+        video.classList.add ("selfie");
+     } else
+        video.classList.remove ("selfie")
+  }
 
 
     /*
